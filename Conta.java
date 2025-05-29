dbdc54b2167bf18ae3538d0a5d49883d649bb7a7
Luigi <luigitorlaiokano@gmail.com>
2025-05-28 23:47:40 -0300

    Create Conta.java

---
+++ Conta.java
@@ -0,0 +1,47 @@
+import java.io.Serializable;
+
+public abstract class Conta implements Serializable {
+    protected String numero;
+    protected double saldo;
+    protected String tipo;
+
+    public Conta(String numero, String tipo) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.saldo = 0.0;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+
+    public boolean sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferir(Conta destino, double valor) {
+        if (sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+}
